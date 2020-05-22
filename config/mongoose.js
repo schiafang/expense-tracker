8ca25e0ac9374e425e6ea2a602394408637e069e
carey <schiafang@gmail.com>
2020-05-22 19:05:35 +0800

    fix: console error: move dateValue from main.js to new.hbs

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const db = mongoose.connection
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-record-list'
 
-mongoose.connect(process.env.MONGODB_URI || MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 db.on('error', () => console.log('MongoDB error!'))
 db.once('open', () => console.log('MongoDB connected!'))
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,3 @@ listItem.forEach(item => {
   if (Number(item.dataset.id) % 2 === 0) { item.style.backgroundColor = `rgb(80, 80, 80, 0.1)` }
   if (Number(item.dataset.id) % 2 === 1) { item.style.backgroundColor = `rgb(80, 80, 80, 0.2` }
 })
-
-const dateValue = document.querySelector('.new-date')
-const now = new Date()
-dateValue.value = now.toISOString().slice(0, 10)
